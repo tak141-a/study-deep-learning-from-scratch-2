14cc5ad320fc623730a69bf3032d947958aa8c37
tak141-a <tak_wstmp@icloud.com>
2021-02-18 23:08:19 +0900

    Add TwoLayerNet class

--- ch01/forward_net.py
+++ ch01/forward_net.py
@@ -50,6 +50,66 @@ class Affine:
         Args:
             x (list): 入力
         """
-        W, b = self.params      # paramsからWとbを取り出し
+        W, b = self.params  # paramsからWとbを取り出し
         out = np.dot(x, W) + b  # xW + b
         return out
+
+
+class TwoLayerNet:
+    """2層レイヤ
+
+    (IN) -> Affine -> Sigmoid -> Affine -> (OUT)
+
+    Attributes:
+        layers (list): レイヤ
+        params (list): 全ての学習パラメータ
+    """
+
+    def __init__(self, input_size, hidden_size, output_size):
+        """
+        Params:
+            input_size (int): 入力層のサイズ
+            hidden_size (int): 隠れ層のサイズ
+            output_size (int): 出力層のサイズ
+        """
+        I, H, O = input_size, hidden_size, output_size
+
+        # 重みとバイアスの初期化
+        W1 = np.random.randn(I, H)
+        b1 = np.random.randn(H)
+        W2 = np.random.randn(H, O)
+        b2 = np.random.randn(O)
+
+        # レイヤの生成
+        self.layers = [
+            Affine(W1, b1),
+            Sigmoid(),
+            Affine(W2, b2)
+        ]
+
+        # すべての重みをリストにまとめる(各レイヤのインスタンス変数paramsを連結)
+        self.params = []
+        for layer in self.layers:
+            self.params += layer.params
+
+    def predict(self, x):
+        """推論
+
+        TwoLayerNetの推論を行う
+
+        Params:
+            x (list): 入力
+
+        Returns:
+            list: TwoLayerNetの推論結果
+        """
+        for layer in self.layers:
+            x = layer.forward(x)
+        return x
+
+
+# 動作確認
+x = np.random.randn(10, 2)
+model = TwoLayerNet(2, 4, 3)
+s = model.predict(x)
+print(model.params)
