a1f1f651dc366e8efbd7f69239ae7afab1cb7111
tak141-a <tak_wstmp@icloud.com>
2021-03-22 11:06:52 +0900

    add MatMul layer

--- common/layers.py
+++ common/layers.py
@@ -1,4 +1,5 @@
-from common.functions import softmax, cross_entropy_error
+import numpy as np
+from functions import softmax, cross_entropy_error
 
 
 class SoftmaxWithLoss:
@@ -34,3 +35,55 @@ class SoftmaxWithLoss:
 
         loss = cross_entropy_error(self.y, self.t)
         return loss
+
+
+class MatMul:
+    """
+    MatMulレイヤ(行列の積)
+
+    Attributes:
+        params (list): 学習するパラメータ
+        grads (list): 勾配
+        x (list): 入力
+    """
+    def __init__(self, W):
+        """
+        params:
+            W (list): 重み
+        """
+        self.params = [W]
+        self.grads = [np.zeros_like(W)]  # paramsと同じ形状のゼロ配列を作る
+        self.x = None
+
+    def forward(self, x):
+        """順伝播
+
+        MatMulレイヤの順伝播の結果を返す
+
+        Args:
+            x (ndarray): 入力
+        """
+        W, = self.params
+        out = np.dot(x, W)
+        self.x = x
+        return out
+
+    def backward(self, dout):
+        """逆伝播
+
+        MatMulレイヤの逆伝播の結果を返す
+
+        Args:
+            dout ():
+        """
+        W, = self.params
+        dx = np.dot(dout, W.T)
+        dW = np.dot(self.x.T, dout)
+        self.grads[0][...] = dW  # NumPy配列のメモリ位置を固定した上でNumPy配列の要素を上書き
+        return dx
+
+
+W = np.random.rand(2, 3)
+x = np.random.rand(3, 2)
+matmul = MatMul(W)
+print(matmul.forward(x))
