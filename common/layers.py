dcbb8b65cfe5b4cadf22944108ed2989dcad8b88
tak141-a <tak_wstmp@icloud.com>
2021-03-22 11:38:16 +0900

    add affine layer

--- common/layers.py
+++ common/layers.py
@@ -126,6 +126,62 @@ class Sigmoid:
         return dx
 
 
+class Affine:
+    """
+    Affineレイヤ
+
+    Attributes:
+        params (list): 学習するパラメータ
+        grads (list): 勾配
+        out (list): 順伝播の出力
+    """
+    def __init__(self, W, b):
+        """
+        params:
+            W (ndarray): 重み
+            b (): バイアス
+        """
+        self.params = [W, b]
+        self.grads = [np.zeros_like(W), np.zeros_like(b)]
+        self.x = None
+
+    def forward(self, x):
+        """順伝播
+
+        Affineレイヤの順伝播の結果を返す
+
+        Args:
+            x (ndarray): 入力
+
+        Return:
+            out (ndarray): 順伝播結果
+        """
+        W, b = self.params
+        out = np.dot(x, W) + b  # 入力と重みの行列の積 + バイアス
+        self.x = x
+        return out
+
+    def backward(self, dout):
+        """逆伝播
+
+        Affineレイヤの逆伝播の結果を返す
+
+        Args:
+            dout (ndarray): 入力
+
+        Return
+            dx (ndarray): 逆伝播結果
+        """
+        W, b = self.params
+        dx = np.dot(dout, W.T)
+        dW = np.dot(self.x.T, dout)
+        db = np.sum(dout, axis=0)
+
+        self.grads[0][...] = dW
+        self.grads[1][...] = db
+        return dx
+
+
 W = np.random.rand(2, 3)
 x = np.random.rand(3, 2)
 sigmoid = Sigmoid()
